af31066950ba24de4eb4ae7ef642d728fee72008
Jabili Adepu <jabili.adepu@gmail.com>
2021-01-23 09:44:29 +0530

    Niyuj Project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CareertipsComponent } from './careertips/careertips.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'careertips', component: CareertipsComponent},
+  {path: 'companies', component: CompaniesComponent},
+  {path: '', redirectTo: '/home', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,37 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+//import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { CareertipsComponent } from './careertips/careertips.component';
+import { CompaniesComponent } from './companies/companies.component';
+//import { FooterComponent } from './footer/footer.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    //HeaderComponent,
+    HomeComponent,
+    CareertipsComponent,
+    CompaniesComponent,
+    //FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
